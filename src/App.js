3423216340bd7940b05c2a2c027dae2a34ea5b64
Vincent Sit <vincentsit00@ymail.com>
2023-04-04 07:37:31 +1200

    Update basic web UI

--- src/App.js
+++ src/App.js
@@ -1,65 +1,79 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import "./App.css";
+import Calendar from './components/Calendar/index';
+
+const userId = "1";
+const url = "https://localhost:7227/api";
+
+function findWorkDays (daysCount, includeWeekends) {
+  const days = [];
+  
+  // temp set the most amount of cards you can show is 14
+  let count = daysCount;
+
+  for (var i=0; i<daysCount*2 && count > 0; i++) {    
+    const today = new Date();
+    today.setDate(today.getDate() + i);
+    const dayIndex = today.getDay();
+    
+    if (includeWeekends || dayIndex != 0 && dayIndex != 6) {
+      days.push(i);
+      count--;      
+    }
+  }  
+  return days;
+}
 
 const App = () => {
+  const [userDisplayName, setUserDisplayName] = useState("user");
+  const [daysToShow, setDaysToShow] = useState(5);
+  const [includeWeekends, setIncludeWeekends] = useState(false);
+  // useEffect to get the daysToShow and includeWeekends for the user
+  
+  // useEffect to get displayName from user ID and update displayName
+  useEffect(() => {
+    async function getUserDataById(url, id) {
+      const fullUrl = `${url}/users/${id}`;
+      await fetch(fullUrl)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error("Error encountered. Check id.");
+        })
+        .then(data => {
+          setUserDisplayName(data.displayName);
+        })
+        .catch(error => {
+          alert("There was a problem fetching the data. ", error);
+        });
+    }
+    
+    getUserDataById(url, userId);
+  });
+
+  useEffect(() => {
+    const getDaysToShowAndIncludeWeekends = async (url, id) => {
+      const fullUrl = `${url}/users/${id}`;
+      const response = await fetch(fullUrl);
+      const data = await response.json();
+      setDaysToShow(Math.min(data.daysToShow, 14));
+      setIncludeWeekends(data.includeWeekends);
+    };
+  
+    getDaysToShowAndIncludeWeekends(url, userId);
+
+  });
+
+  
   return (
     <>
-      <body>
-        <h1>Where are you working this week?</h1>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name"/><br/><br/>
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email"/><br/><br/>
-
-        <div id="dateContainer">Loading...</div>
-
-        <div className="calendar">
-          <header>
-              <button className="secondary">Today</button>
-              <div className="calendar__title">
-                <div className="icon secondary chevron_left">‹</div>
-                <h1 className=""><span></span><strong>18 JAN – 24 JAN</strong> 2016</h1>
-                <div className="icon secondary chevron_left">›</div>
-              </div> 
-              <div></div>
-          </header>
-          
-          <div className="outer">
-          
-          
-            <table>
-              <thead>
-                <tr>
-                  <th className="headcol"></th>
-                  <th>Mon, 18</th>
-                  <th>Tue, 19</th>
-                  <th className="today">Wed, 20</th>
-                  <th>Thu, 21</th>
-                  <th>Fri, 22</th>
-                </tr>
-              </thead>
-            </table>
-        
-            <div className="wrap"> 
-              <table className="offset">
-            
-                <tbody>
-                  <tr>
-                    <td className="headcol">Work location</td>
-                    <td ></td>      
-                    <td id="dayTwo"></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>      
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      
-      </body>
+      <div>
+        <h1 id="greeting">Hi {userDisplayName}, where are you working for the next {daysToShow} days?</h1>
+        <Calendar days={findWorkDays(daysToShow, includeWeekends)}/>
+      </div>
     </>
 )};
+
 
 export default App;
