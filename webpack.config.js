e5b63a3dbeb915800b1ab991dc2ae9dc50848e35
Vincent Sit <vincentsit00@ymail.com>
2023-04-11 16:43:46 +1200

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,43 @@
-import { resolve as _resolve, join } from 'path';
-import HtmlWebPackPlugin from 'html-webpack-plugin';
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
-export const output = {
-  path: _resolve(__dirname, 'build'),
-  filename: 'bundle.js',
-};
-
-export const resolve = {
-  modules: [join(__dirname, 'src'), 'node_modules'],
-  alias: {
-    react: join(__dirname, 'node_modules', 'react'),
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',    
   },
-};
-
-export const module = {
-  rules: [
-    {
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
+  resolve: {
+    modules: [path.join(__dirname, 'src'), 'node_modules'],
+    alias: {
+      react: path.join(__dirname, 'node_modules', 'react'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
       },
-    },
-    {
-      test: /\.css$/,
-      use: [
-        {
-          loader: 'style-loader',
-        },
-        {
-          loader: 'css-loader',
-        },
-      ],
-    },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+    }),
   ],
+  
 };
-
-export const plugins = [
-  new HtmlWebPackPlugin({
-    template: './src/index.html',
-  }),
-];
